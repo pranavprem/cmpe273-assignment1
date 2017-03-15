840bb5b8a703cdb79dc455d14c6f30c5d7a13625
Pranav Prem <pranavprem93@gmail.com>
2017-03-14 19:53:04 -0700

    Assignment complete. Additional functionality to be implemented

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ MAINTAINER Your Name "pranavprem93@gmail.com"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-ENTRYPOINT ["python"]
-CMD ["app.py"]
+ENTRYPOINT ["python", "app.py"]
+CMD ["https://github.com/pranavprem/cmpe273-assignment1"]
--- app.py
+++ app.py
@@ -2,11 +2,19 @@ from flask import Flask
 from flask import jsonify
 from github import Github
 from github import UnknownObjectException
+from github import RateLimitExceededException
+import sys
 import yaml
 
-url="https://github.com/sithu/assignment1-config-example".split("/")
 
+app = Flask(__name__)
+#print sys.argv[1]
+
+url=sys.argv[1].split("/")
+set_response_flag=False
 config_file="dev-config"
+
+#print url
 
 for i in range(len(url)):
     if "github" in url[i]:
@@ -14,25 +22,46 @@ for i in range(len(url)):
         repo=url[i+2]
         break
 
-
-print user,repo
-lastModified=open("last_commit.txt").read()
-
-
-app = Flask(__name__)
-
-git=Github().get_user(user).get_repo(repo)
-try:
-    response=yaml.safe_load(git.get_file_contents(config_file+".yml").content.decode(git.get_contents(config_file+".yml").encoding))
-except UnknownObjectException as e:
+if(i==len(url)):
+    response = "url supplied is not github"
+else:
+    #print user,repo
     try:
-        response=yaml.safe_load(git.get_file_contents(config_file+".json").content.decode(git.get_contents(config_file+".json").encoding))
+        git=Github().get_user(user).get_repo(repo)
     except UnknownObjectException:
-        response="The file requested was not found as json or yml. Please do not use file extension in file name"
-except Exception as e:
-    print e
+        response = "repository does not exist. Please check link supplied"
+        set_response_flag=True
+    except RateLimitExceededException:
+        print "reached here"
+        if set_response_flag==False:
+            response = "You have reached your github rate limit. Try after an hour."
+            set_response_flag=True
+    except Exception as e:
+        if set_response_flag==False:
+            response ="Please contact the developer for error: %s" % e
+            set_response_flag=True
 
-print response
+    try:
+        if set_response_flag==False:
+            response=yaml.safe_load(git.get_file_contents(config_file+".yml").content.decode(git.get_contents(config_file+".yml").encoding))
+            set_response_flag=True
+    except UnknownObjectException as e:
+        try:
+            if set_response_flag==False:
+                response=yaml.safe_load(git.get_file_contents(config_file+".json").content.decode(git.get_contents(config_file+".json").encoding))
+                set_response_flag=True
+        except UnknownObjectException:
+            if set_response_flag==False:
+                response="The file requested was not found as json or yml. Please do not use file extension in file name"
+                set_response_flag=True
+    except Exception as e:
+        if set_response_flag==False:
+            response ="Please contact the developer for error: %s" % e
+            set_response_flag=True
+
+#print response
+
+
 
 
 @app.route("/")
@@ -40,4 +69,5 @@ def hello():
     return jsonify(response)
 
 if __name__ == "__main__":
+    app.config["url"] = sys.argv[1]
     app.run(debug=True,host='0.0.0.0')
